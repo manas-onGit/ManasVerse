cc950567e0f014f98a2bb0322afb97341a5c831a
Manas Srivastava <manasworks04@gmail.com>
2025-07-25 00:46:43 +0530

    Auto run migrate on wsgi startup for Render

--- Ecommerce/wsgi.py
+++ Ecommerce/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for Ecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
+
+# Initialize Django
+django.setup()
+
+# Run migrations automatically on startup (Render workaround)
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
 
 application = get_wsgi_application()
